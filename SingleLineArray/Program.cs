9d9fb882c52ed3cd5fb68ae0713adc49bc20141a
msunkist99 <msunkist99@gmail.com>
2019-01-28 19:03:58 -0600

    20190129InitialCode

--- SingleLineArray/Program.cs
+++ SingleLineArray/Program.cs
@@ -7,9 +7,13 @@ namespace SingleLineArray
         public static void Main()
         {
             // TODO: Initialize a single line an array with the following values: 1, 1, 2, 3, 5, 8. 
+            int[] numbers = new int[6] { 1, 1, 2, 3, 5, 8 };
 
             // TODO: Loop through the array using foreach and print out each value on its own line.
-
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
             Console.ReadLine();
         }
     }
